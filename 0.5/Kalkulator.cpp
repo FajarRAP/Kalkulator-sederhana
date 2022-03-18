a7d402496a275ac9006066b40f6911ede9109853
TititUpin <fajary781@gmail.com>
2022-03-18 11:34:18 +0700

    first

--- 0.5/Kalkulator.cpp
+++ 0.5/Kalkulator.cpp
@@ -2,31 +2,37 @@
 using namespace std;
 
 int main(){
-	int angka1, angka2, hasil;
+	float angka, hasil=0;
 	char pilih;
+
+	operasi:
+	system("cls");
 	cout<<"Masukkan angka : ";
-	cin>>angka1;
+	cin>>angka;
 	cout<<"Pilih operasi  : "<<endl;
-	cout<<"\t1. +\t2. -\t3. x\t4. :"<<endl;
+	cout<<"\t1. +\t2. -\t3. x\t4. : \t5. ="<<endl;
 	cin>>pilih;
-	cout<<"Masukkan angka : ";
-	cin>>angka2;
 
 	if(pilih=='1'||pilih=='+'){
-		hasil=angka1+angka2;
-		cout<<"Hasilnya = "<<hasil<<endl;
+		hasil+=angka;
+		goto operasi;
 	}else if(pilih=='2'||pilih=='-'){
-		hasil=angka1-angka2;
-		cout<<"Hasilnya = "<<hasil<<endl;
+		hasil-=angka;
+		goto operasi;
 	}else if(pilih=='3'||pilih=='x'){
-		hasil=angka1*angka2;
-		cout<<"Hasilnya = "<<hasil<<endl;
+		hasil*=angka;
+		goto operasi;
 	}else if(pilih=='4'||pilih==':'){
-		hasil=angka1/angka2;
-		cout<<"Hasilnya = "<<hasil<<endl;
-	}else{
+		hasil/=angka;
+		goto operasi;
+	}else if(pilih=='5'||pilih=='='){
+		system("cls");
+		cout<<"Hasilnya : "<<hasil<<endl;
+	}
+	else{
 		cout<<"Pilihan tidak tersedia"<<endl;
 	}
+
 	cin.get();
 	return 0;
 }
